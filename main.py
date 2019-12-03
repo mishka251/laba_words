19fb579729e5a41ddfbe193f328e8d077bb8b08f
Mikhail Belov <mishkabelka251@gmail.com>
2019-12-04 02:12:41 +0500

    Вывод в файл

--- main.py
+++ main.py
@@ -53,13 +53,7 @@ import seaborn as sns
 
 from keras import backend as K
 
-# %matplotlib inline
-
-# from tensorflow.python.client import device_lib
-# print(device_lib.list_local_devices())
-#
-# import tensorflow as ts
-# sess = ts.compat.v1.Session(config=ts.compat.v1.ConfigProto(log_device_placement=True))
+from timeit import default_timer as timer
 
 print('done')
 
@@ -74,30 +68,6 @@ stopwords = set(stopwords.words('english'))
 X = X.apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
 Y = films["tag"]
 
-# reviews = pd.read_csv('AllProductReviews.csv')
-# infos = pd.read_csv('ProductInfo.csv')
-#
-#
-# def normalize_star(star):
-#     if star in (1, 2):
-#         return 'neg'
-#     if star in (4, 5):
-#         return 'pos'
-#     return 'neutral'
-#
-# reviews["ReviewStar"] = reviews["ReviewStar"].apply(normalize_star)
-#
-# reviews["ReviewBody"] = reviews["ReviewBody"].apply(lambda x: BeautifulSoup(x, "lxml").get_text())
-# reviews["ReviewBody"] = reviews["ReviewBody"].apply(lambda x: x.lower())
-# reviews["ReviewBody"] = reviews["ReviewBody"].apply(lambda x: re.sub("[^a-zA-Z]", " ", x))
-# reviews["ReviewBody"] = reviews["ReviewBody"].apply(lambda x: re.sub("\s+", " ", x))
-# stopwords = set(stopwords.words('english'))
-# reviews["ReviewBody"] = reviews["ReviewBody"].apply(lambda x: ' '.join([word for word in x.split() if
-# word not in stopwords]))
-#
-# X = reviews["ReviewBody"]
-# Y = reviews["ReviewStar"]
-
 label_encoder = LabelEncoder()
 Y = label_encoder.fit_transform(Y)
 Y = to_categorical(Y)
@@ -110,70 +80,77 @@ max_len = 150
 tokenizer = Tokenizer(num_words=max_words)
 tokenizer.fit_on_texts(X_train)
 
-X_train_seq = tokenizer.texts_to_sequences(X_train)
-X_train_seq = sequence.pad_sequences(X_train_seq, maxlen=max_len)
+dropouts = [0.35, 0.35, 0.45]
+lstm_cnts = [64, 32, 32]
 
-model = Sequential()
-model.add(Embedding(max_words, 100, input_length=max_len))
-model.add(LSTM(64))  # количество нейронов
-model.add(Dropout(0.35))  # вероятность
-model.add(Dense(2, activation=K.tanh))
-# model.add(Activation('softmax'))
+times_f = open("times.txt", "w")
 
-# learning_rate = 0.001
-optimizer = Adam(0.001)
-# Компиляция данных
-model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+for (lstm, dropout) in zip(lstm_cnts, dropouts):
+    start = timer()
+    X_train_seq = tokenizer.texts_to_sequences(X_train)
+    X_train_seq = sequence.pad_sequences(X_train_seq, maxlen=max_len)
 
-verbose = 1
-epochs = 10
-batch_size = 128
-validation_split = 0.2
+    model = Sequential()
+    model.add(Embedding(max_words, 100, input_length=max_len))
+    model.add(LSTM(lstm))  # количество нейронов
+    model.add(Dropout(dropout))  # вероятность
+    model.add(Dense(2, activation=K.sigmoid))
+    # model.add(Activation('softmax'))
 
-history = model.fit(
-    X_train_seq,
-    Y_train,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=verbose,
-    validation_split=validation_split
-)
+    # learning_rate = 0.001
+    optimizer = Adam(0.001)
+    # Компиляция данных
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-history_dict = history.history
-loss_values = history_dict['loss']
-val_loss_values = history_dict['val_loss']
+    verbose = 1
+    epochs = 10
+    batch_size = 128
+    validation_split = 0.2
 
-epochs = range(1, len(history_dict['accuracy']) + 1)
+    history = model.fit(
+        X_train_seq,
+        Y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        validation_split=validation_split
+    )
 
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-plt.xlabel('Epohs')
-plt.ylabel('Loss')
-plt.title("tanh 0.35")
-plt.legend()
-plt.show()
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
 
-# plt.clf()
+    epochs = range(1, len(history_dict['accuracy']) + 1)
 
-acc_values = history_dict['accuracy']
-val_acc_values = history_dict['val_accuracy']
+    plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+    plt.xlabel('Epohs')
+    plt.ylabel('Loss')
+    plt.title(f"sigmoid dropout={dropout} lstm={lstm}")
+    plt.legend()
+    plt.show()
 
-plt.plot(epochs, acc_values, 'bo', label='Training acc')
-plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
-plt.xlabel('Epohs')
-plt.ylabel('Accuracy')
-plt.title("tanh 0.35")
-plt.legend()
-plt.show()
+    acc_values = history_dict['accuracy']
+    val_acc_values = history_dict['val_accuracy']
 
-test_X_seq = tokenizer.texts_to_sequences(X_test)
-test_X_seq = sequence.pad_sequences(test_X_seq, maxlen=max_len)
-results = model.evaluate(test_X_seq, Y_test)
-print('test loss, test acc:', results)
+    plt.plot(epochs, acc_values, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+    plt.xlabel('Epohs')
+    plt.ylabel('Accuracy')
+    plt.title(f"sigmoid dropout={dropout} lstm={lstm}")
+    plt.legend()
+    plt.show()
 
-ypreds = model.predict_classes(test_X_seq, verbose=1)
+    test_X_seq = tokenizer.texts_to_sequences(X_test)
+    test_X_seq = sequence.pad_sequences(test_X_seq, maxlen=max_len)
+    results = model.evaluate(test_X_seq, Y_test)
+    print('test loss, test acc:', results)
 
-print(ypreds)
+    ypreds = model.predict_classes(test_X_seq, verbose=1)
 
-model.save("model" + str(datetime.datetime.now().microsecond) + ".h5")
+    print(ypreds)
+
+    model.save("model" + str(datetime.datetime.now().microsecond) + ".h5")
+    times_f.write(f"time = {timer()-start}")
+
 print("end")
